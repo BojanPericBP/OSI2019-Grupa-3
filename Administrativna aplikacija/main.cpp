31ab18141647d74f5c709dae89814bb472ae53da
WhiteJava <bojan.peric67@gmail.com>
2019-12-18 16:28:19 +0100

    Uradjeni templates za konfiguracijske filove

--- Administrativna aplikacija/main.cpp
+++ Administrativna aplikacija/main.cpp
@@ -1,4 +1,13 @@
+#include <stdio.h>
+#pragma warning(disable : 4996)
 int main()
 {
+	FILE* fp;
+
+	if ((fp = fopen("../config files/test.txt", "r")) != NULL)
+		printf("otoreno");
+	else
+		printf("nije otvoreno");
+
 	return 0;
 }
--- Korisnicka aplikacija/Main.cpp
+++ Korisnicka aplikacija/Main.cpp
@@ -1,4 +1,13 @@
+#include <stdio.h>
 int main()
 {
+
+	FILE* fp;
+
+	if ((fp = fopen("test.txt", "r")) != NULL)
+		printf("otoreno");
+	else
+		printf("nije otvoreno");
+
 	return 0;
 }
