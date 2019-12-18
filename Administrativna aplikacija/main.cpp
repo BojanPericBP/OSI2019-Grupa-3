3c0543d0330049b76244510208a38e5e51895f59
WhiteJava <bojan.peric67@gmail.com>
2019-12-18 16:49:47 +0100

    Dodati hederi za obje aplikacije

--- Administrativna aplikacija/main.cpp
+++ Administrativna aplikacija/main.cpp
@@ -2,12 +2,7 @@
 #pragma warning(disable : 4996)
 int main()
 {
-	FILE* fp;
 
-	if ((fp = fopen("../config files/test.txt", "r")) != NULL)
-		printf("otoreno");
-	else
-		printf("nije otvoreno");
 
 	return 0;
 }
--- Korisnicka aplikacija/Main.cpp
+++ Korisnicka aplikacija/Main.cpp
@@ -2,12 +2,5 @@
 int main()
 {
 
-	FILE* fp;
-
-	if ((fp = fopen("test.txt", "r")) != NULL)
-		printf("otoreno");
-	else
-		printf("nije otvoreno");
-
 	return 0;
 }
