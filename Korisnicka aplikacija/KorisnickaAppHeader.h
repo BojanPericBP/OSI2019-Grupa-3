a7145c5ec4f681cfbc35f34fa525cd6ee628271b
KristinaKrickovicx99 <kristina.x99@gmail.com>
2019-12-23 19:47:18 +0100

    Ispravljene greske pri korisnickom unosu u glavnom meniju

--- Korisnicka aplikacija/KorisnickaAppHeader.h
+++ Korisnicka aplikacija/KorisnickaAppHeader.h
@@ -16,7 +16,7 @@ void glavni_meni_korisnicke_aplikacije()
 {
 	system("cls");
 	system("color f0");
-	printf("\n\n******************************************************************DOBRODOSLI******************************************************************\n\n");
+	printf("\n\n***************************************************************** DOBRODOSLI *****************************************************************\n\n");
 	printf("                                          Da biste odabrali zeljenu opciju unesite odgovarajuci broj!\n");
 	printf("     ____________________________________________________________________________________________________________________________________     \n\n");
 	printf("                     1: PREPORUCENI DOGADJAJI\n");
@@ -34,7 +34,7 @@ void meni_prikaz_preporucenih_dogadjaja()
 	system("cls");
 	system("color 0e");
 
-	printf("\n*******************************************************PREGLED PREPORUCENIH DOGADJAJA*********************************************************\n");
+	printf("\n****************************************************** PREGLED PREPORUCENIH DOGADJAJA ********************************************************\n");
 	//ispis preporucenih dogadjaja
 	printf("\n**********************************************************************************************************************************************\n\n");
 }
@@ -44,7 +44,7 @@ void meni_svih_dogadjaja()
 	system("cls");
 	system("color 4f");
 
-	printf("\n************************************************************PREGLED SVIH DOGADJAJA************************************************************\n");
+	printf("\n*********************************************************** PREGLED SVIH DOGADJAJA ***********************************************************\n");
 	//funkcija za ispis svih dogadjaja
 	printf("\n**********************************************************************************************************************************************\n\n");
 }
@@ -53,7 +53,7 @@ void meni_kviza()
 {
 	system("cls");
 	system("color 0b");
-	printf("\n**********************************************************DOBRO DOSLI U KVIZ******************************************************************\n");
+	printf("\n********************************************************* DOBRO DOSLI U KVIZ *****************************************************************\n");
 	//igranje kviza
 	printf("\n**********************************************************************************************************************************************\n\n");
 	printf("Unesite M -> Nazad na pocetni meni\n\tE -> Izlaz iz programa\n");
@@ -64,7 +64,7 @@ void meni_rang_liste()
 	system("cls");
 	system("color 5f");
 
-	printf("\n*****************************************************************RANG LISTA*******************************************************************\n");
+	printf("\n**************************************************************** RANG LISTA ******************************************************************\n");
 	//prikaz rang liste
 	printf("\n**********************************************************************************************************************************************\n\n");
 	printf("Unesite M -> Nazad na pocetni meni\n\tE -> Izlaz iz programa\n");
@@ -75,7 +75,7 @@ void meni_help()
 	system("cls");
 	system("color 70");
 
-	printf("\n*******************************************************************POMOC********************************************************************\n");
+	printf("\n****************************************************************** POMOC *********************************************************************\n");
 	printf("\nAplikacija 'Sta se radi na estradi' je aplikacija koja omogucava pracenje dogadjaja u nekom gradu.\nOmogucava korisniku da pregleda koji se sve dogadjai odrzavaju u nekom gradu, datum i mjesto dorzavanja dogadjaja,\nkao i komentare drugih korisnika o predstojecim dogadjajima.\n"
 		"Za detaljne informacije pogledajte korisnicku dokumentaziju.\n");
 	printf("\n**********************************************************************************************************************************************\n\n");
@@ -88,14 +88,12 @@ void meni_o_korisnickoj_aplikaciji()
 	system("cls");
 	system("color 0a");
 
-	printf("\n****************************************************************O aplikaciji****************************************************************\n");
+	printf("\n*************************************************************** O aplikaciji *****************************************************************\n");
 	printf("\nAplikacija 'Sta se radi na estradi' je aplikacija koja omogucava pracenje dogadjaja u nekom gradu.\nOmogucava korisniku da pregleda koji se sve dogadjai odrzavaju u nekom gradu, datum i mjesto dorzavanja dogadjaja,\nkao i komentare drugih korisnika o predstojecim dogadjajima.\n"
 		"\nApliakciju su radili studenti Elektrotehnickog fakulteta Banjaluka :\n"
 	"Jelena Jakovljevic (Jeca Pereca),\nKristina Krickovic (Kricka),\nSasa Stankovic (Sale),\n"
 		"Bojan Janjic (Che),\nBojan Peric (Boki, Bole, Boss)\n");
 	printf("\n**********************************************************************************************************************************************\n\n");
-
-	printf("Unesite D -> Pregled korisnicke dokumentacije\n\tM -> Nazad na pocetni meni\n\tE -> Izlaz iz programa\n");
 }
 
 void odabir_opcije_korisnika()
@@ -103,13 +101,14 @@ void odabir_opcije_korisnika()
 	char a[50] = {};
 	char c[50] = {};
 	char flag = 0;
+	FILE* fp=NULL;
 	while (1)
 	{
-		scanf("%s", a);
+		scanf("\n%[^\n]s", a);
 		while (a[1] != 0)
 		{
-			printf("Nepostojeca opcija!\nUnesite ponovo: ");
-			scanf("%s", a);
+			printf("\nNepostojeca opcija!\nUnesite ponovo: ");
+			scanf("\n%[^\n]s", a);
 		}
 		switch (a[0])
 		{
@@ -254,20 +253,29 @@ void odabir_opcije_korisnika()
 		case '6':
 			system("cls");
 			meni_o_korisnickoj_aplikaciji();
-			scanf("\n%s", c);
-			while ((c[0] != 'M' && c[0] != 'm' && c[0] != 'E' && c[0] != 'e') || c[1] != 0)
+			do 
 			{
-				printf("Nepostojeca opcija! Izaberite ponovo: \n");
+				printf("Unesite D -> Pregled korisnicke dokumentacije\n\tM -> Nazad na pocetni meni\n\tE -> Izlaz iz programa\n");
 				scanf("\n%s", c);
-			}
-			if (c[0] == 'M' || c[0] == 'm')
-			{
-				system("cls");
-				glavni_meni_korisnicke_aplikacije();
-			}
-			else if (c[0] == 'E' || c[0] == 'e')
-				exit(1);
-
+				while ((c[0] != 'M' && c[0] != 'm' && c[0] != 'E' && c[0] != 'e' && c[0] != 'D' && c[0] != 'd') || c[1] != 0)
+				{
+					printf("Nepostojeca opcija! Izaberite ponovo: \n");
+					scanf("\n%s", c);
+				}
+				if (c[0] == 'M' || c[0] == 'm')
+				{
+					system("cls");
+					glavni_meni_korisnicke_aplikacije();
+					flag = 1;
+				}
+				else if (c[0] == 'E' || c[0] == 'e')
+					exit(1);
+				else if (c[0] == 'D' || c[0] == 'd')
+				{
+					flag = 0;
+					system("\"\"../config files/Korisnicka dokumentacija/Dokumentacija.docx\"\"");
+				}
+			} while (!flag);
 			break;
 		default:
 			printf("Nepostojeca opcija!\n");
