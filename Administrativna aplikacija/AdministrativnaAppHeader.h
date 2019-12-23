786d54e37d047369cf3f2ea3f8665a2a955242a9
WhiteJava <bojan.peric67@gmail.com>
2019-12-23 22:53:16 +0100

    Uradjeni meniji do kraja, dodat header za administrativnu aplikaciju i validacija korisnickog unosa

--- Administrativna aplikacija/AdministrativnaAppHeader.h
+++ Administrativna aplikacija/AdministrativnaAppHeader.h
@@ -20,8 +20,6 @@ void glavni_meni_administrativne_aplikacije()
 	printf("     ____________________________________________________________________________________________________________________________________     \n\n");
 	printf("                     6: IZLAZ\n");
 	printf("\n**********************************************************************************************************************************************\n\n");
-
-
 }
 
 void meni_logovanje_administratora()
@@ -29,41 +27,40 @@ void meni_logovanje_administratora()
 	system("color f0");
 	printf("\n*******************************************************************PRIJAVA********************************************************************\n\n");
 	//funkcija za logovanje administratora u kojoj se nalazi funkcija za provjeru validnosti pristupnih podataka
-	printf("Unesite M -> Nazad na pocetni meni\n\tE -> Izlaz iz programa\n");
+
 }
 
 void meni_dodavanje_dogadjaja()
 {
 	system("color 0a");
 	printf("\n**************************************************************DODAVANJE DOGADJAJA*************************************************************\n\n");
-	printf("Unesite M -> Nazad na pocetni meni\n\tE -> Izlaz iz programa\n");
+
 }
 void meni_uredjivanje_dogadjaja()
 {
 	system("color 0a");
 	printf("\n*************************************************************UREDJIVANJE DOGADJAJA************************************************************\n\n");
-	printf("Unesite M -> Nazad na pocetni meni\n\tE -> Izlaz iz programa\n");
+
 }
 
 void meni_brisanje_dogadjaja()
 {
 	system("color 0a");
 	printf("\n**************************************************************BRISANJE DOGADJAJA**************************************************************\n\n");
-	printf("Unesite M -> Nazad na pocetni meni\n\tE -> Izlaz iz programa\n");
+
 }
 
 void meni_dodavanje_kategorije()
 {
 	system("color 0a");
 	printf("\n*************************************************************DODAVANJE KATEGORIJE*************************************************************\n\n");
-	printf("Unesite M -> Nazad na pocetni meni\n\tE -> Izlaz iz programa\n");
+
 }
 
 void meni_uredjivanje_kviza()
 {
 	system("color 0a");
 	printf("\n**************************************************************UREDJIVANJE KVIZA**************************************************************\n\n");
-	printf("Unesite M -> Nazad na pocetni meni\n\tE -> Izlaz iz programa\n");
 }
 
 void odabir_opcije_admina()
@@ -76,11 +73,11 @@ void odabir_opcije_admina()
 	while (1)
 	{
 		
-		scanf("\n%s", &a);
+		scanf("\n%[^\n]s", a);
 		while (a[1] != 0)
 		{
 			printf("Nepostojeca opcija!\nUnesite ponovo: ");
-			scanf("%s", &a);
+			scanf("\n%[^\n]s", a);
 		}
 		switch (a[0])
 		{
@@ -88,51 +85,49 @@ void odabir_opcije_admina()
 			system("cls");
 			meni_dodavanje_dogadjaja();
 			
-			scanf("\n%s", &c);
-			while (c[1] != 0 || (c[0] != 'm' && c[0] != 'M' && c[0] != 'e' && c[0] != 'E'))
+			scanf("\n%[^\n]s", c);
+			while ((c[0] != 'M' && c[0] != 'm' && c[0] != 'E' && c[0] != 'e') || c[1] != 0)
 			{
-				printf("Nepostojeca opcija!\nUnesite: ");
-				scanf("\n%s", &c);
+				printf("Nepostojeca opcija! Izaberite ponovo: \n");
+				scanf("\n%[^\n]s", c);
 			}
-
-				if (c[0] == 'M' || c[0] == 'm')
-				{
-					system("cls");
-					glavni_meni_administrativne_aplikacije();
-				}
-				else if (c[0] == 'E' || c[0] == 'e')
-					exit(1);
-				else
-					printf("Nepostojeca opcija!\n");
+			if (c[0] == 'M' || c[0] == 'm')
+			{
+				system("cls");
+				glavni_meni_administrativne_aplikacije();
+			}
+			else if (c[0] == 'E' || c[0] == 'e')
+				exit(1);
 
 			break;
 		case '2':
 			system("cls");
-			meni_uredjivanje_dogadjaja();
+			meni_dodavanje_dogadjaja();
 
-			scanf("\n%s", &c);
-			while (c[1] != 0 || ( c[0] != 'm' && c[0] != 'M' && c[0] != 'e' && c[0] != 'E'))
+			scanf("\n%[^\n]s", c);
+
+			while ((c[0] != 'M' && c[0] != 'm' && c[0] != 'E' && c[0] != 'e') || c[1] != 0)
 			{
-				printf("Nepostojeca opcija!\nUnesite: ");
-				scanf("\n%s", &c);
+				printf("Nepostojeca opcija! Izaberite ponovo: \n");
+				scanf("\n%[^\n]s", c);
 			}
-				if (c[0] == 'M' || c[0] == 'm')
-				{
-					system("cls");
-					glavni_meni_administrativne_aplikacije();
-				}
-				else if (c[0] == 'E' || c[0] == 'e')
-					exit(1);
+			if (c[0] == 'M' || c[0] == 'm')
+			{
+				system("cls");
+				glavni_meni_administrativne_aplikacije();
+			}
+			else if (c[0] == 'E' || c[0] == 'e')
+				exit(1);
 			break;
 		case '3':
 			system("cls");
 			meni_brisanje_dogadjaja();
 
-			scanf("\n%s", &c);
-			while (c[1] != 0 || (c[0] != 'm' && c[0] != 'M' && c[0] != 'e' && c[0] != 'E'))
+			scanf("\n%[^\n]s", c);
+			while ((c[0] != 'M' && c[0] != 'm' && c[0] != 'E' && c[0] != 'e') || c[1] != 0)
 			{
-				printf("Nepostojeca opcija!\nUnesite: ");
-				scanf("\n%s", &c);
+				printf("Nepostojeca opcija! Izaberite ponovo: \n");
+				scanf("\n%[^\n]s", c);
 			}
 			if (c[0] == 'M' || c[0] == 'm')
 			{
@@ -146,37 +141,37 @@ void odabir_opcije_admina()
 		case '4':
 			system("cls");
 			meni_dodavanje_kategorije();
-			scanf("\n%s", &c);
-			while (c[1] != 0 || (c[0] != 'm' && c[0] != 'M' && c[0] != 'e' && c[0] != 'E'))
+			scanf("\n%[^\n]s", c);
+			while ((c[0] != 'M' && c[0] != 'm' && c[0] != 'E' && c[0] != 'e') || c[1] != 0)
 			{
-				printf("Nepostojeca opcija!\nUnesite: ");
-				scanf("\n%s", &c);
+				printf("Nepostojeca opcija! Izaberite ponovo: \n");
+				scanf("\n%[^\n]s", c);
 			}
-				if (c[0] == 'M' || c[0] == 'm')
-				{
-					system("cls");
-					glavni_meni_administrativne_aplikacije();
-				}
-				else if (c[0] == 'E' || c[0] == 'e')
-					exit(1);
+			if (c[0] == 'M' || c[0] == 'm')
+			{
+				system("cls");
+				glavni_meni_administrativne_aplikacije();
+			}
+			else if (c[0] == 'E' || c[0] == 'e')
+				exit(1);
 				break;
 		case '5':
 			system("cls");
 			meni_uredjivanje_kviza();
 
-			scanf("\n%s", &c);
-			while (c[1] != 0 || (c[0] != 'm' && c[0] != 'M' && c[0] != 'e' && c[0] != 'E'))
+			scanf("\n%[^\n]s", c);
+			while ((c[0] != 'M' && c[0] != 'm' && c[0] != 'E' && c[0] != 'e') || c[1] != 0)
 			{
-				printf("Nepostojeca opcija!\nUnesite: ");
-				scanf("%s", &c);
+				printf("Nepostojeca opcija! Izaberite ponovo: \n");
+				scanf("\n%[^\n]s", c);
 			}
-				if (c[0] == 'M' || c[0] == 'm')
-				{
-					system("cls");
-					glavni_meni_administrativne_aplikacije();
-				}
-				else if (c[0] == 'E' || c[0] == 'e')
-					exit(1);
+			if (c[0] == 'M' || c[0] == 'm')
+			{
+				system("cls");
+				glavni_meni_administrativne_aplikacije();
+			}
+			else if (c[0] == 'E' || c[0] == 'e')
+				exit(1);
 				break;
 		case '6':
 			exit(1);
